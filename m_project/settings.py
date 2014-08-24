925724bc6470217d45fd7c36168e72e08ff875ef
Vic Bui <quangvu88@hotmail.com>
2014-08-24 18:50:27 +0800

    setting templates done

--- m_project/settings.py
+++ m_project/settings.py
@@ -80,4 +80,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+# media 
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+#templates dir 
+#PROJECT_PATH = os.path.join(BASE_DIR,os.pardir)
+#PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATE_PATH = os.path.join(BASE_DIR,'templates')
+
+TEMPLATE_DIRS =     (
+    TEMPLATE_PATH,
+)
+
